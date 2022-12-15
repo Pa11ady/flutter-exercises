d78be9b8934e12ee3e055072aff25ee0ad48d874
Pavel <pa11ady@yandex.ru>
2022-12-15 18:25:57 +0300

    Weather finish

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -39,7 +39,13 @@ Widget _buildBody() {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    _weatherDescription()
+                    _weatherDescription(),
+                    Divider(),
+                    _temperature(),
+                    Divider(),
+                    _temperatureForecast(),
+                    Divider(),
+                    _footerRatings()
                   ],
                 ),
               )
@@ -66,10 +72,87 @@ Column _weatherDescription() {
             fontWeight: FontWeight.bold),
       ),
       Divider(),
-      Text("temperatures increase from 20°C to 23°C and it is overcast"
-          "In Moscow during June average daily high "
-          "or mostly cloudy about 47% of the time.",
+      Text("The Moscow weather will be mostly clear with a few low clouds. "
+          "Probability of precipitation 0%. Air pressure normal "
+          "Air temperature  20°C. Gentle winds (3—4 km/h)",
         style: TextStyle(color: Colors.black54),)
+    ],
+  );
+}
+
+Row _temperature() {
+  return Row(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+              Icons.wb_sunny,
+              color: Colors.yellow
+          ),
+        ],
+      ),
+      SizedBox(width: 16.0),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text("20°C Clear", style: TextStyle(color: Colors.deepPurple),)
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text("Moscow region, Moscow", style: TextStyle(color: Colors.grey),)
+            ],
+          )
+        ],
+      ),
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 10.0,
+    children:
+    List.generate(7, (int index) {
+      return Chip(
+        label: Text(
+          '${index + 19}°C',
+          style: TextStyle(fontSize: 15.0),
+        ),
+        avatar: Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+          side: BorderSide(color: Colors.grey)
+        ),
+        backgroundColor: Colors.grey.shade100,
+      );
+    }),
+  );
+}
+
+Row _footerRatings() {
+  var stars = Row(
+    children: <Widget>[
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[500]),
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[500]),
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[500]),
+      Icon(Icons.star_border, size: 15.0, color: Colors.yellow[500]),
+      Icon(Icons.star_border, size: 15.0, color: Colors.yellow[500])
+    ],
+  );
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: <Widget>[
+      Text("Info with weather.com", style: TextStyle(fontSize: 15)),
+      stars
     ],
   );
 }
