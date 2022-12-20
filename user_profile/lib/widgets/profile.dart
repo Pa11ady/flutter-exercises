200cf372b86c2f0ee3130bd5ddefdae59f106426
Pavel <pa11ady@yandex.ru>
2022-12-20 16:26:25 +0300

    user_profile menu part 2

--- user_profile/lib/widgets/profile.dart
+++ user_profile/lib/widgets/profile.dart
@@ -6,18 +6,14 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+        backgroundColor: Colors.grey,
         appBar: AppBar(title: Text('Настройка')),
         body: Container(
           width: double.infinity,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _UserInfo(),
-              SizedBox(height: 30),
-              _MenuBlock()
-            ],
+            children: [_UserInfo(), SizedBox(height: 30), _MenuBlock()],
           ),
         ));
   }
@@ -29,27 +25,43 @@ class _MenuBlock extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      width: double.infinity,
-      child: Column(children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-            Icon(Icons.favorite_border_outlined),
-            SizedBox(width: 15),
-            Expanded(child: Text('Избранное')),
-            //SizedBox(width: 15),
-            Icon(Icons.chevron_right),
-
-          ],),
-        )
-      ],)
-    );
+        color: Colors.white,
+        width: double.infinity,
+        child: Column(
+          children: [
+            _MenuRow(icon: Icons.favorite, text: 'Избраное'),
+            _MenuRow(icon: Icons.call, text: 'Звонки'),
+            _MenuRow(icon: Icons.computer, text: 'Устройства'),
+            _MenuRow(icon: Icons.folder, text: 'Папка с чатами'),
+          ],
+        ));
   }
 }
 
+class _MenuRow extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const _MenuRow({Key? key, required this.icon, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Icon(icon),
+          SizedBox(width: 15),
+          Expanded(child: Text(text)),
+          //SizedBox(width: 15),
+          Icon(Icons.chevron_right),
+        ],
+      ),
+    );
+  }
+}
 
 class _UserInfo extends StatelessWidget {
   const _UserInfo({Key? key}) : super(key: key);
@@ -75,7 +87,6 @@ class _UserInfo extends StatelessWidget {
   }
 }
 
-
 class _NickNameWidget extends StatelessWidget {
   const _NickNameWidget({
     Key? key,
@@ -84,8 +95,7 @@ class _NickNameWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text('@petrov777',
-        style: TextStyle(
-            color: Colors.grey, fontSize: 17));
+        style: TextStyle(color: Colors.grey, fontSize: 17));
   }
 }
 
@@ -110,8 +120,7 @@ class _UserPhoneWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text('+7(777)777 77 77',
-        style: TextStyle(
-            color: Colors.grey, fontSize: 15));
+        style: TextStyle(color: Colors.grey, fontSize: 15));
   }
 }
 
