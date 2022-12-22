71694bbb67266f54aedb4a114e82df6b16bb01dd
Pavel <pa11ady@yandex.ru>
2022-12-22 19:23:13 +0300

    weather_forecas finish

--- weather_forecast/lib/main.dart
+++ weather_forecast/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(
               bottomOpacity: 0.0,
               elevation: 0.0,
-              title: Text('Weather Forcast',
+              title: Text('Weather Forecast',
                   style: TextStyle(
                       color: Colors.white, fontWeight: FontWeight.w600)),
               centerTitle: true,
@@ -40,16 +40,16 @@ Widget _buildBody() {
                 style: TextStyle(color: Colors.white),
               )),
         ),
-        SizedBox(height: 10.0),
-        _cityDetail(),
-        SizedBox(height: 10.0),
-        _temperatureDetail(),
-        SizedBox(height: 10.0),
-        _extraWeatherDetail(),
         SizedBox(height: 20.0),
+        _cityDetail(),
+        SizedBox(height: 20.0),
+        _temperatureDetail(),
+        SizedBox(height: 20.0),
+        _extraWeatherDetail(),
+        SizedBox(height: 40.0),
         Text('7-DAY WEATHER FORECAST',
-            style: TextStyle(color: Colors.white, fontSize: 16)),
-        SizedBox(height: 10.0),
+            style: TextStyle(color: Colors.white, fontSize: 16.0)),
+        SizedBox(height: 5.0),
         _dayWeatherForecast()
       ],
     ),
@@ -57,17 +57,156 @@ Widget _buildBody() {
 }
 
 Widget _cityDetail() {
-  return Container();
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Text('Murmansk Oblast, RU',
+            style: TextStyle(color: Colors.white, fontSize: 32.0)),
+        SizedBox(height: 10.0),
+        Text('Friday, Mar 20, 2020',
+            style: TextStyle(color: Colors.white, fontSize: 20.0))
+      ],
+    ),
+  );
 }
 
 Widget _temperatureDetail() {
-  return Container();
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+          Icon(
+            Icons.wb_sunny,
+            color: Colors.white,
+            size: 60.0,
+          )
+        ]),
+        SizedBox(width: 16.0),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text('14 °F',
+                    style: TextStyle(color: Colors.white, fontSize: 35.0))
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Text('LIGTH SNOW',
+                    style: TextStyle(color: Colors.white, fontSize: 20.0))
+              ],
+            )
+          ],
+        )
+      ],
+    ),
+  );
 }
 
 Widget _extraWeatherDetail() {
-  return Container();
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          children: <Widget>[
+            Icon(
+              Icons.ac_unit,
+              color: Colors.white,
+              size: 30.0,
+            ),
+            SizedBox(height: 5),
+            Text('5', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+            Text('km/hr', style: TextStyle(color: Colors.white, fontSize: 14.0))
+          ],
+        ),
+        Column(
+          children: <Widget>[
+            Icon(
+              Icons.ac_unit,
+              color: Colors.white,
+              size: 30.0,
+            ),
+            SizedBox(height: 5),
+            Text('3', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+            Text('%', style: TextStyle(color: Colors.white, fontSize: 14.0))
+          ],
+        ),
+        Column(
+          children: <Widget>[
+            Icon(
+              Icons.ac_unit,
+              color: Colors.white,
+              size: 30.0,
+            ),
+            SizedBox(height: 5),
+            Text('20', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+            Text('%', style: TextStyle(color: Colors.white, fontSize: 14.0))
+          ],
+        )
+      ],
+    ),
+  );
 }
 
 Widget _dayWeatherForecast() {
-  return Container();
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: SizedBox(
+      //margin: const EdgeInsets.symmetric(vertical: 20.0),
+      height: 120.0,
+      child: ListView(
+        // This next line does the trick.
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          _listElement('Monday', 13, Icons.ac_unit),
+          _listElement('Tuesday', 12, Icons.sunny_snowing),
+          _listElement('Wednesday', 10, Icons.sunny_snowing),
+          _listElement('Thursday', 11, Icons.sunny_snowing),
+          _listElement('Friday', 14, Icons.sunny),
+          _listElement('Saturday', 15, Icons.ac_unit),
+          _listElement('Sunday', 16, Icons.sunny),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _listElement(String day, int temperature, IconData icon) {
+  return Padding(
+    padding: const EdgeInsets.all(10.0),
+    child: Container(
+      width: 140.0,
+      color: Colors.red,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(day, style: TextStyle(color: Colors.white, fontSize: 25.0)),
+          SizedBox(height: 8),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('$temperature °F',
+                  style: TextStyle(color: Colors.white, fontSize: 30.0)),
+              SizedBox(width: 5.0),
+              Icon(
+                icon,
+                color: Colors.white,
+                size: 30.0,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
 }
